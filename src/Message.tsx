e74b1cf1b46c06ab15c985b142fd74e751544ea9
camkol <koliyus3@hotmail.com>
2023-09-21 18:41:08 +0200

    state,hooks,props

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,9 @@
 // PascalCasing
 function Message() {
-    // JSX: JavaScript XML
-    const name = 'Cam';
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>;
+  // JSX: JavaScript XML
+  const name = "Cam";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
 }
 
-export default Message; 
+export default Message;
